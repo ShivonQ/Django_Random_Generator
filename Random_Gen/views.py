2069c1dcd975525bb59e3f03b9d4e8d78643d065
ShivonQ <realitykeeper@gmail.com>
2016-11-09 21:19:51 -0600

    Trying to get a integer to pass to a second page

--- Random_Gen/views.py
+++ Random_Gen/views.py
@@ -5,25 +5,27 @@ from Random_Gen.models import *
 coins = TreasureCoinsBaseValue
 goods = TreasureGoodsBaseValue
 items = TreasureItemsBaseResults
-base_results = [coins, goods, items]
+base_results=[coins,goods,items]
 
 
 def treasure_result(request):
     if request.method == 'POST':
-        level = request.POST.get('enc_level', 1)
+        enc_level = request.POST.get('enc_level',1)
     else:
-        level = 1
+        enc_level = 1
 
     result = []
     for model in base_results:
 
         dice_roll = randint(1, 100)
-        this_model = (model.objects.filter(level == level))
+        # model.objects
+        these_models = model(level=enc_level)
+        for thing in these_models:
+            print(thing)
         print(this_model)
         result.append(this_model)
 
     return render(request, 'treasure_result.html', result)
-
 
 def base(request):
     title = {'title': 'Welcome to the Treasure Generator!'}
