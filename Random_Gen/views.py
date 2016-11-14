6fa3a0cc7750cbf56f40538578cf8599cec24ef1
timmymill <timmymill87@gmail.com>
2016-11-14 13:41:34 -0600

    Created modular method to use for each model to return the row from the database for each model that is to be used for each model's method

--- Random_Gen/views.py
+++ Random_Gen/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 # from django.shortcuts import *
 # import pdb
 from Random_Gen.models import *
-from .models import TreasureItemsBaseResults
 from django.db import models
+from .models import TreasureItemsBaseResults
+from .models import *
 
 coins = TreasureCoinsBaseValue
 goods = TreasureGoodsBaseValue
@@ -11,15 +12,30 @@ items = TreasureItemsBaseResults
 base_results = [coins, goods, items]
 
 
-def treasure_result(request):
+def treasure_result(request, item=''):
     if request.method == 'POST':
         encounter_level = request.POST.get('enc_level', 1)
-    # else:
-    #     encounter_level = 1
+    else:
+        encounter_level = 1
 
     # result = {'result': []}
     # second_list = []
-    # for model in base_results:
+    for index in range(1, len(base_results)):
+        #
+        random_percentage = randint(1, 100)
+
+        if index == 0:
+            coin = get_model(random_percentage, encounter_level, coins)
+            get_coin_result(coin)
+
+        if index == 1:
+
+            get_goods_result()
+
+        if index == 2:
+            item = get_model(random_percentage, encounter_level, items)
+            item = get_item_result(item)
+
         # for model.coins take die size and number of die, for each number in number_of_die:randint(1,die_size)
         # add the die rolls together and multiply them by the multiplier
         # add on the coin_type, pack into a dictionary
@@ -30,44 +46,40 @@ def treasure_result(request):
         # Depending on which is True make that the item type
 
         # all three are packed into a dictionary and sent to the html page to be displayed, maybe with a title.
-        dice_roll = randint(1, 100)
-        item = get_item_result(dice_roll, encounter_level)
-        # model.objects
 
-        # result['result'].append(get_item_result(dice_roll, encounter_level))
-        # these_models = model.objects.filter(level=encounter_level).values()
-        #
-        # for thing in these_models:
-        #     p_up = thing['percent_upper']
-        #     p_dn = thing['percent_lower']
-        #     if p_dn <= dice_roll <= p_up:
-        #         result['result'].append(thing)
-        #         # print(these_models)
-        # # result.append(these_models)
+    # model.objects
 
-        return render(request, 'treasure_result.html', {"item": item})
-
-
-def get_item_result(percentage, encounter_level, item_type=''):
-    selected_item = models.Model
+    # result['result'].append(get_item_result(dice_roll, encounter_level))
+    # these_models = model.objects.filter(level=encounter_level).values()
     #
-    for item in items.objects.filter(level=encounter_level).values():
-        #
-        if item['percent_lower'] <= percentage <= item['percent_upper']:
-            selected_item = item
+    # for thing in these_models:
+    #     p_up = thing['percent_upper']
+    #     p_dn = thing['percent_lower']
+    #     if p_dn <= dice_roll <= p_up:
+    #         result['result'].append(thing)
+    #         # print(these_models)
+    # # result.append(these_models)
 
-    # then check for the 'True' value in the 4 booleans.
-    # Depending on which is True make that the item type
-    # item_types = [selected_item['items_type_mundane'], selected_item['items_type_minor'],
-    #               selected_item['items_type_medium'], selected_item['items_type_major']]
-    # item_types = [{'mundane item': selected_item['items_type_mundane']},
-    #               {'minor item': selected_item['items_type_minor']},
-    #               {'medium item': selected_item['items_type_medium']},
-    #               {'major item': selected_item['items_type_major']}]
-    item_types = {'mundane item': selected_item['items_type_mundane'],
-                  'minor item': selected_item['items_type_minor'],
-                  'medium item': selected_item['items_type_medium'],
-                  'major item': selected_item['items_type_major']}
+    return render(request, 'treasure_result.html', {"item": item})
+
+
+def get_model(random_percentage, encounter_level, model):
+    for result in model.objects.filter(level=encounter_level).values():
+
+        if result['percent_lower'] <= random_percentage <= result['percent_upper']:
+            return result
+
+
+def get_goods_result():
+    pass
+
+
+def get_item_result(item, item_type=''):
+
+    item_types = {'mundane item': item['items_type_mundane'],
+                  'minor item': item['items_type_minor'],
+                  'medium item': item['items_type_medium'],
+                  'major item': item['items_type_major']}
 
     for key, boolean in item_types.items():
         if boolean:
@@ -79,19 +91,19 @@ def get_item_result(percentage, encounter_level, item_type=''):
     return {'item_type': item_type}
 
 
-def get_coin_result(percentage, encounter_level):
-    possible_coin_rows = items.objects.filter(level=encounter_level).values()
-    selected_coin = None
-    #
-    for coin in coins.objects.filter(level=encounter_level).values():
-        #
-        if coin['percent_lower'] <= percentage <= coin['percent_upper']:
-            selected_coin = coin
-            # item_id = item['id']
+def get_coin_result(coin):
+    # possible_coin_rows = items.objects.filter(level=encounter_level).values()
+    # selected_coin = None
+    # #
+    # for coin in coins.objects.filter(level=encounter_level).values():
+    #     #
+    #     if coin['percent_lower'] <= percentage <= coin['percent_upper']:
+    #         selected_coin = coin
+    #         # item_id = item['id']
 
-    number_of_die = selected_coin['number_of_die']
-    die_size = selected_coin['die_size']
-    multiplier = selected_coin['multiplier']
+    number_of_die = coin['number_of_die']
+    die_size = coin['die_size']
+    multiplier = coin['multiplier']
 
     # for each dice in die, randint(1, die_size)
     roll_total = 0
